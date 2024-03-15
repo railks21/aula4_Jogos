33c2f63cf13af2585baea8fd0532f1b2933041a8
railks21 <josepedro.abreu21@gmail.com>
2024-03-15 12:22:10 +0000

    Files

--- sokoban/Game1.cs
+++ sokoban/Game1.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
+using System.IO;
 
 namespace sokoban
 {
@@ -9,6 +10,12 @@ namespace sokoban
         private GraphicsDeviceManager _graphics;
         private SpriteBatch _spriteBatch;
         private SpriteFont font;
+        private int nrLinhas = 0;
+        private int nrColunas = 0;
+        private char[,] level;
+        private Texture2D player, dot, box, wall; //Load images Texture
+        int tileSize = 64; //potencias de 2 (operações binárias)
+        private Player sokoban;
 
         public Game1()
         {
@@ -20,6 +27,11 @@ namespace sokoban
         protected override void Initialize()
         {
             // TODO: Add your initialization logic here
+            LoadLevel("level1.txt");
+
+            _graphics.PreferredBackBufferHeight = tileSize * level.GetLength(1); //definição da altura
+            _graphics.PreferredBackBufferWidth = tileSize * level.GetLength(0); //definição da largura
+            _graphics.ApplyChanges(); //aplica a atualização da janela
 
             base.Initialize();
         }
@@ -28,6 +40,10 @@ namespace sokoban
         {
             _spriteBatch = new SpriteBatch(GraphicsDevice);
             font = Content.Load<SpriteFont>("File"); //Use the name of sprite font file ('File')
+            player = Content.Load<Texture2D>("Character4");
+            dot = Content.Load<Texture2D>("EndPoint_Red");
+            box = Content.Load<Texture2D>("CrateDark_Brown");
+            wall = Content.Load<Texture2D>("Wall_Black");
 
             // TODO: use this.Content to load your game content here
         }
@@ -49,10 +65,69 @@ namespace sokoban
             // TODO: Add your drawing code here
 
             _spriteBatch.Begin();
-            _spriteBatch.DrawString(font, "O texto que quiser", new Vector2(100, 10), Color.Black);
+
+            _spriteBatch.DrawString(font, "O texto que quiser", new Vector2(0, 0), Color.Black);
+            _spriteBatch.DrawString(font, $"Numero de Linhas = {nrLinhas}", new Vector2(0, 20), Color.Black);
+            _spriteBatch.DrawString(font, $"Numero de Colunas = {nrColunas}", new Vector2(0, 40), Color.Black);
+
+            Rectangle position = new Rectangle(0, 0, tileSize, tileSize); //calculo do retangulo a depender do tileSize
+            for (int x = 0; x < level.GetLength(0); x++) //pega a primeira dimensão
+            {
+                for (int y = 0; y < level.GetLength(1); y++) //pega a segunda dimensão
+                {
+                    position.X = x * tileSize; // define o position
+                    position.Y = y * tileSize; // define o position
+
+                    switch (level[x, y])
+                    {
+                        //case 'Y':
+                        //    _spriteBatch.Draw(player, position, Color.White);
+                        //    break;
+                        case '#':
+                            _spriteBatch.Draw(box, position, Color.White);
+                            break;
+                        case '.':
+                            _spriteBatch.Draw(dot, position, Color.White);
+                            break;
+                        case 'X':
+                            _spriteBatch.Draw(wall, position, Color.White);
+                            break;
+                    }
+                }
+            }
+
+            position.X = sokoban.Position.X * tileSize; //posição do Player
+            position.Y = sokoban.Position.Y * tileSize; //posição do Player
+            _spriteBatch.Draw(player, position, Color.White); //desenha o Player
+
             _spriteBatch.End();
 
             base.Draw(gameTime);
+        }
+
+        void LoadLevel(string levelFile)
+        {
+            string[] linhas = File.ReadAllLines($"Content/{levelFile}"); // "Content/" + level
+            nrLinhas = linhas.Length;
+            nrColunas = linhas[0].Length;
+
+            level = new char[nrColunas, nrLinhas];
+
+            for (int x = 0; x < nrColunas; x++)
+            {
+                for (int y = 0; y < nrLinhas; y++)
+                {
+                    if (linhas[y][x] == 'Y')
+                    {
+                        sokoban = new Player(x, y);
+                        level[x, y] = ' '; // put a blank instead of the sokoban 'Y'
+                    }
+                    else
+                    {
+                        level[x, y] = linhas[y][x];
+                    }
+                }
+            }
         }
     }
 }
